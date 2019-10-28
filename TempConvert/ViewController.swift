d8895339de44974087551333bc35aba320141f1c
Artjoms Vorona <artjomsvorona@gmail.com>
2019-10-28 11:13:17 +0200

    IBActions, IBOutlets added Navigation Bar Button added

--- TempConvert/ViewController.swift
+++ TempConvert/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var tempSlider: UISlider!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func tempSliderMoved(_ sender: Any) {
+    }
+    
 
 }
 
